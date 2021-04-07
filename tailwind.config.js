f4edb878f418292f236d83556e9259d8f93510a2
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2021-04-06 22:25:44 -0400

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    boxShadow: {
-      '2xl' : '0 35px 60px -40px rgba(0, 0, 0, 0.5)'
-    },
-    extend: {},
+  extend: {},
   },
   variants: {
     extend: {},
